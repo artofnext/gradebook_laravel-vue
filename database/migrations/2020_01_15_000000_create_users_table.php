068853e968de5b9b2cdda26da18e2dc7ed911276
artofnext <novnovalex@gmail.com>
2020-01-16 15:09:47 +0200

    foreing key bugfix

--- database/migrations/2020_01_15_000000_create_users_table.php
+++ database/migrations/2020_01_15_000000_create_users_table.php
@@ -21,6 +21,8 @@ class CreateUsersTable extends Migration
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
+            $table->unsignedBigInteger('role_id');
+            $table->foreign('role_id')->references('id')->on('roles');
         });
     }
 
--- database/migrations/2020_01_16_120008_create_permission_roles_table.php
+++ database/migrations/2020_01_16_120008_create_permission_roles_table.php
@@ -14,16 +14,19 @@ class CreatePermissionRolesTable extends Migration
     public function up()
     {
         Schema::create('permission_roles', function (Blueprint $table) {
-            $table->unsignedInteger('permission_id');
-            $table->unsignedInteger('role_id');
+            $table->unsignedBigInteger('permission_id');
+            $table->unsignedBigInteger('role_id');
+
             $table->foreign('permission_id')
                 ->references('id')
                 ->on('permissions')
                 ->onDelete('cascade');
+
             $table->foreign('role_id')
                 ->references('id')
                 ->on('roles')
                 ->onDelete('cascade');
+
             $table->primary(['permission_id', 'role_id']);
         });
     }
