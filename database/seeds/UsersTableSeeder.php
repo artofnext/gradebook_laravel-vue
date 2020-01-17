4b7b70e6f8456641a78a15fbde573bc9f6364367
artofnext <novnovalex@gmail.com>
2020-01-17 09:34:48 +0200

    workable seeders

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class UsersTableSeeder extends Seeder
 {
@@ -12,14 +14,34 @@ class UsersTableSeeder extends Seeder
     public function run()
     {
         DB::table('users')->insert([
-            'name' => 'User1',
+            'name' => 'Alex',
             'email' => 'user1@email.com',
             'password' => bcrypt('password'),
+            'role_id' => DB::table('roles')->pluck('id')[0], //role admin
         ]);
         DB::table('users')->insert([
-            'name' => 'user2',
+            'name' => 'Alex User',
             'email' => 'user2@email.com',
             'password' => bcrypt('password'),
+            'role_id' => DB::table('roles')->pluck('id')[1], //role user
+        ]);
+        DB::table('users')->insert([
+            'name' => 'Alex Teacher',
+            'email' => 'user3@email.com',
+            'password' => bcrypt('password'),
+            'role_id' => DB::table('roles')->pluck('id')[2], //role teacher
+        ]);
+        DB::table('users')->insert([
+            'name' => 'Alex Student',
+            'email' => 'user4@email.com',
+            'password' => bcrypt('password'),
+            'role_id' => DB::table('roles')->pluck('id')[3], //role student
+        ]);
+        DB::table('users')->insert([
+            'name' => 'Alex Guest',
+            'email' => 'user5@email.com',
+            'password' => bcrypt('password'),
+            'role_id' => DB::table('roles')->pluck('id')[4], //role guest
         ]);
     }
 }
